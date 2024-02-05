b942cd159e16c9ea2b3787d70c01ee7c28facb70
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-02-05 22:35:23 +0100

    Added new fit modules to simplify fitting routine

--- src/sipmanalyze/__init__.py
+++ src/sipmanalyze/__init__.py
@@ -5,5 +5,8 @@ __version__ = version.__version__
 import sys
 
 if sys.version_info.major < 3:
-  import warnings
-  warnings.warn("Only supports python3!")
+    import warnings
+
+    warnings.warn("Only supports python3!")
+
+from . import estimate, fit, formats
